2954313c65bbd0bacc3fac97f28201a2a56ca5de
Ayesha <ayesha_04@yahoo.com>
2017-07-03 16:09:18 +0000

    Step2 of Chunks Assignment

---
+++ PrintWithBufferedStatement.js
@@ -0,0 +1,29 @@
+var https = require('https');
+
+function getAndPrintBufferedStatement() {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+
+
+    var chunk = "";
+    response.on('data', function(data) {
+      chunk += data;
+
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+      console.log(chunk);
+    });
+
+  });
+};
+
+getAndPrintBufferedStatement();
+
